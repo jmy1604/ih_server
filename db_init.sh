fe11b682990d0ce8be8dce97d70a253584d28f17
huoshan017 <huoshan017@hotmail.com>
2018-08-27 16:34:17 +0800

    mm

---
+++ db_init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+HOSTNAME="127.0.0.1"
+PORT="3306"
+USERNAME="root"
+PASSWORD=""
+DBNAMES=("ih_center_server" "ih_hall_server" "ih_hall_server2")
+
+for var in ${DBNAMES[@]};
+do
+	mysql -h${HOSTNAME} -P${PORT}  -u${USERNAME} -p${PASSWORD} -e "create database IF NOT EXISTS $var"
+done
