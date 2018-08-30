0be5469bac21e9a10557ba1422a54d5355b0cf5e
huoshan017 <huoshan017@hotmail.com>
2018-08-30 16:13:34 +0800

    mm

--- src/hall_server/chat.go
+++ src/hall_server/chat.go
@@ -285,22 +285,22 @@ func (this *Player) pull_chat(channel int32) int32 {
 	pull_time, _ := this.db.Chats.GetLastPullTime(channel)
 	if now_time-pull_time < pull_msg_cooldown {
 		log.Error("Player[%v] pull channel[%v] chat msg is cooling down", this.Id, channel)
-		response := &msg_client_message.S2CChatMsgPullResponse{}
-		this.Send(uint16(msg_client_message_id.MSGID_S2C_CHAT_MSG_PULL_RESPONSE), response)
-		return 1
+		//response := &msg_client_message.S2CChatMsgPullResponse{}
+		//this.Send(uint16(msg_client_message_id.MSGID_S2C_CHAT_MSG_PULL_RESPONSE), response)
+		return int32(msg_client_message.E_ERR_CHAT_PULL_COOLING_DOWN)
 	}
 
 	msgs := chat_mgr.pull_chat(this)
-	if msgs != nil && len(msgs) > 0 {
-		if !this.db.Chats.HasIndex(channel) {
-			this.db.Chats.Add(&dbPlayerChatData{
-				Channel:      channel,
-				LastPullTime: now_time,
-			})
-		} else {
-			this.db.Chats.SetLastPullTime(channel, now_time)
-		}
+	//if msgs != nil && len(msgs) > 0 {
+	if !this.db.Chats.HasIndex(channel) {
+		this.db.Chats.Add(&dbPlayerChatData{
+			Channel:      channel,
+			LastPullTime: now_time,
+		})
+	} else {
+		this.db.Chats.SetLastPullTime(channel, now_time)
 	}
+	//}
 
 	response := &msg_client_message.S2CChatMsgPullResponse{
 		Channel: channel,
