4fa700c42e9c93b3f812c626bd9aa237d0d7596b
huoshan017 <huoshan017@hotmail.com>
2018-10-18 15:47:50 +0800

    11111

---
+++ src/table_config/table_system_unlock.go
@@ -0,0 +1,80 @@
+package table_config
+
+import (
+	"encoding/xml"
+	"ih_server/libs/log"
+	"ih_server/src/server_config"
+	"io/ioutil"
+)
+
+type XmlSystemUnlockItem struct {
+	ServerId string `xml:"ServerID,attr"`
+	Level    int32  `xml:"Group,attr"`
+}
+
+type XmlSystemUnlockConfig struct {
+	Items []XmlSystemUnlockItem `xml:"item"`
+}
+
+type SystemUnlockTableMgr struct {
+	Map   map[string]*XmlSystemUnlockItem
+	Array []*XmlSystemUnlockItem
+}
+
+func (this *SystemUnlockTableMgr) Init(table_file string) bool {
+	if !this.Load(table_file) {
+		log.Error("SystemUnlockTableMgr Init load failed !")
+		return false
+	}
+	return true
+}
+
+func (this *SystemUnlockTableMgr) Load(table_file string) bool {
+	if table_file == "" {
+		table_file = "SystemUnlock.xml"
+	}
+	table_path := server_config.GetGameDataPathFile(table_file)
+	data, err := ioutil.ReadFile(table_path)
+	if nil != err {
+		log.Error("SystemUnlockTableMgr read file err[%s] !", err.Error())
+		return false
+	}
+
+	tmp_cfg := &XmlSystemUnlockConfig{}
+	err = xml.Unmarshal(data, tmp_cfg)
+	if nil != err {
+		log.Error("SystemUnlockTableMgr xml Unmarshal failed error [%s] !", err.Error())
+		return false
+	}
+
+	if this.Map == nil {
+		this.Map = make(map[string]*XmlSystemUnlockItem)
+	}
+	if this.Array == nil {
+		this.Array = make([]*XmlSystemUnlockItem, 0)
+	}
+
+	tmp_len := int32(len(tmp_cfg.Items))
+
+	var tmp_item *XmlSystemUnlockItem
+	for idx := int32(0); idx < tmp_len; idx++ {
+		tmp_item = &tmp_cfg.Items[idx]
+
+		this.Map[tmp_item.ServerId] = tmp_item
+		this.Array = append(this.Array, tmp_item)
+	}
+
+	return true
+}
+
+func (this *SystemUnlockTableMgr) Get(id string) *XmlSystemUnlockItem {
+	return this.Map[id]
+}
+
+func (this *SystemUnlockTableMgr) GetUnlockLevel(id string) int32 {
+	item := this.Map[id]
+	if item != nil {
+		return item.Level
+	}
+	return 0
+}
