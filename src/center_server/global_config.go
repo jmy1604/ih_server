65395b2135fabcc20ec8e5f4a38e649b23fe0caf
huoshan017 <huoshan017@hotmail.com>
2018-10-18 11:23:30 +0800

    11111

--- src/center_server/global_config.go
+++ src/center_server/global_config.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"ih_server/libs/log"
+	"ih_server/src/server_config"
 	"io/ioutil"
 )
 
@@ -38,7 +39,7 @@ type GlobalConfig struct {
 var global_config GlobalConfig
 
 func global_config_load() bool {
-	data, err := ioutil.ReadFile("../src/ih_server/game_data/global.json")
+	data, err := ioutil.ReadFile(server_config.GetGameDataPathFile("global.json"))
 	if nil != err {
 		log.Error("global_config_load failed to readfile err(%s)!", err.Error())
 		return false
