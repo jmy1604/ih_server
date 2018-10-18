42ef0d3e8e2d6725dc6ef5eb4c23ed6d32b3ee40
huoshan017 <huoshan017@hotmail.com>
2018-10-18 18:14:35 +0800

    mm

--- pack.sh
+++ pack.sh
@@ -3,20 +3,15 @@ mkdir ih_server
 cd ih_server
 mkdir bin
 
-cp ../../../bin/center_server ./bin
-cp ../../../bin/login_server ./bin
-cp ../../../bin/hall_server ./bin
-cp ../../../bin/rpc_server ./bin
-cp ../../../bin/test_client ./bin
+cp ../bin/center_server ./bin
+cp ../bin/login_server ./bin
+cp ../bin/hall_server ./bin
+cp ../bin/rpc_server ./bin
+cp ../bin/test_client ./bin
 
-mkdir run
-cd run
-mkdir ih_server
-
-cd ../
-cp -r ../../../run/ih_server/conf run/ih_server
-cp -r ../../../run/ih_server/game_data run/ih_server
-cp -r ../../../run/ih_server/sh_script run/ih_server
+cp -r ../conf ./ 
+cp -r ../game_data ./
+cp -r ../sh_script ./
 cd ../
 
 tar -czvf ih_server.tar.gz ih_server
