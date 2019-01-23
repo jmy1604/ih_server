4455e027896bf0239746c982dc46114d5d82e6b7
huoshan017 <huoshan017@hotmail.com>
2019-01-23 11:25:06 +0800

    11111

--- src/login_server/login_server.go
+++ src/login_server/login_server.go
@@ -463,7 +463,12 @@ func bind_new_account_handler(server_id int32, account, password, new_account, n
 	}
 	row.SetRegisterTime(register_time)
 	row.SetUniqueId(uid)
-	row.SetLastSelectServerId(last_server_id)
+	b, client_os := account_get_client_os(account)
+	if b && client_os == share_data.CLIENT_OS_IOS {
+		row.SetLastSelectIOSServerId(last_server_id)
+	} else {
+		row.SetLastSelectServerId(last_server_id)
+	}
 	row.SetOldAccount(account)
 	//dbc.Accounts.RemoveRow(account) // 暂且不删除
 
