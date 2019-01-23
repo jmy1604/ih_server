3ebe1af892ef5a49f5bc4fe25d0dd07d26ac35e3
huoshan017 <huoshan017@hotmail.com>
2019-01-23 17:56:56 +0800

    11111

--- sh_script/start_hall_server_5.sh
+++ sh_script/start_hall_server_5.sh
@@ -2,4 +2,4 @@
 set -x
 
 cd ../bin
-nohup `pwd`/hall_server -f `pwd`/../conf/hall_server5.json 1>/dev/null 2>/dev/null &
+nohup `pwd`/hall_server -f `pwd`/../conf/hall_server5.json 1>/dev/null 2>hs5_err.log &
