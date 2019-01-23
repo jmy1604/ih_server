2ff1a64599991fea1368d9d79d9fbb9050a54d7d
huoshan017 <huoshan017@hotmail.com>
2019-01-23 16:07:46 +0800

    11111

--- sh_script/start_hall_server_3.sh
+++ sh_script/start_hall_server_3.sh
@@ -2,4 +2,4 @@
 set -x
 
 cd ../bin
-nohup `pwd`/hall_server -f `pwd`/../conf/hall_server3.json 1>/dev/null 2>/dev/null &
+nohup `pwd`/hall_server -f `pwd`/../conf/hall_server3.json 1>/dev/null 2>hs3_err.log &
