a94a52d7a71419854e18648e9f13b729f13d5250
huoshan017 <huoshan017@hotmail.com>
2019-01-24 17:14:11 +0800

    11111

--- sh_script/just_restart_all_server.sh
+++ sh_script/just_restart_all_server.sh
@@ -6,12 +6,12 @@ bash ./kill_all_server.sh
 sleep 1s
 
 cd ../bin
-nohup `pwd`/center_server 1>/dev/null 2>cs_err.log &
+nohup env GOTRACEBACK=crash `pwd`/center_server 1>/dev/null 2>>cs_err.log &
 sleep 1s
-nohup `pwd`/rpc_server 1>/dev/null 2>rs_err.log &
+nohup env GOTRACEBACK=crash `pwd`/rpc_server 1>/dev/null 2>>rs_err.log &
 sleep 1s
-nohup `pwd`/hall_server 1>/dev/null 2>hs_err.log &
+nohup env GOTRACEBACK=crash `pwd`/hall_server 1>/dev/null 2>>hs_err.log &
 sleep 1s 
-nohup `pwd`/hall_server -f `pwd`/../conf/hall_server2.json 1>/dev/null 2>hs2_err.log &
+nohup env GOTRACEBACK=crash `pwd`/hall_server -f `pwd`/../conf/hall_server2.json 1>/dev/null 2>>hs2_err.log &
 sleep 1s
-nohup `pwd`/login_server 1>/dev/null 2>ls_err.log &
+nohup env GOTRACEBACK=crash `pwd`/login_server 1>/dev/null 2>>ls_err.log &
--- sh_script/start_hall_server_5.sh
+++ sh_script/start_hall_server_5.sh
@@ -2,4 +2,4 @@
 set -x
 
 cd ../bin
-nohup `pwd`/hall_server -f `pwd`/../conf/hall_server5.json 1>/dev/null 2>hs5_err.log &
+nohup env GOTRACEBACK=crash `pwd`/hall_server -f `pwd`/../conf/hall_server5.json 1>/dev/null 2>>hs5_err.log &
