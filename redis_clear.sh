29bbf4b8b07da01d5d3d9b539ab3cf27535ce67e
huoshan017 <huoshan017@hotmail.com>
2019-01-28 15:50:44 +0800

    11111

--- redis_clear.sh
+++ redis_clear.sh
@@ -3,4 +3,4 @@
 REDIS_HOSTNAME="127.0.0.1"
 REDIS_PORT="6379"
 
-redis-cli -h $REDIS_HOSTNAME -p $REDIS_PORT del "ih:hall_server:google_pay" "ih:hall_server:apple_pay" "ih:share_data:account_player_list" "ih:hall_server:uid_token_key"
+redis-cli -h $REDIS_HOSTNAME -p $REDIS_PORT del "ih:hall_server:google_pay" "ih:hall_server:apple_pay" "ih:share_data:uid_player_list" "ih:hall_server:uid_token_key"
