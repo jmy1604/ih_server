b9e11174e1fbb67658dbd32e51d3f6b10b01ec54
huoshan017 <huoshan017@hotmail.com>
2019-03-01 18:35:31 +0800

    11111

---
+++ src/share_data/server_player_id.go
@@ -0,0 +1,13 @@
+package share_data
+
+func GetServerIdByPlayerId(player_id int32) int32 {
+	return (player_id >> 20) & 0xffff
+}
+
+func GeneratePlayerId(server_id, serial_id int32) int32 {
+	return ((server_id << 20) & 0x7ff00000) | serial_id
+}
+
+func GenerateGuildId(server_id, serial_id int32) int32 {
+	return ((server_id << 20) & 0x7ff00000) | serial_id
+}
