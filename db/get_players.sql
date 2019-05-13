a5205333032405d4c78aa1db654ae0132684b2dc
huoshan017 <huoshan017@hotmail.com>
2019-05-13 18:37:07 +0800

    11111

--- db/get_players.sql
+++ db/get_players.sql
@@ -6,8 +6,8 @@ BEGIN
     DECLARE account VARCHAR(64);
 	DECLARE unique_id VARCHAR(64);
 	DECLARE last_server_id INT;
-	/*DECLARE s1_pid, s2_pid, s3_pid, s4_pid, s5_pid, s6_pid, s7_pid, s8_pid INT DEFAULT 0;
-	DECLARE s1_name, s2_name, s3_name, s4_name, s5_name, s6_name, s7_name, s8_name VARCHAR(32) DEFAULT '';*/
+	DECLARE s1_pid, s2_pid, s3_pid, s4_pid, s5_pid/*, s6_pid, s7_pid, s8_pid*/ INT DEFAULT 0;
+	DECLARE s1_name, s2_name, s3_name, s4_name, s5_name/*, s6_name, s7_name, s8_name*/ VARCHAR(32) DEFAULT '';
     DECLARE done INT DEFAULT 0;
 
 	DECLARE cur CURSOR FOR (SELECT AccountId, UniqueId, LastSelectServerId FROM Accounts GROUP BY UniqueId);
@@ -35,7 +35,7 @@ BEGIN
  		PRIMARY KEY (Account)
  	);
 	
-	SET @s1_pid=0, @s2_pid=0, @s3_pid=0, @s4_pid=0, @s5_pid=0, @s6_pid=0, @s7_pid=0, @s8_pid=0;
+	/*SET @s1_pid=0, @s2_pid=0, @s3_pid=0, @s4_pid=0, @s5_pid=0, @s6_pid=0, @s7_pid=0, @s8_pid=0;
 	SET @s1_name='', @s2_name='', @s3_name='', @s4_name='', @s5_name='', @s6_name='', @s7_name='', @s8_name=''; 
 	
 	SET @s1_sql = 'SELECT PlayerId, Name INTO s1_pid, s1_name FROM ih_hall_server.Players WHERE UniqueId=unique_id';
@@ -48,7 +48,7 @@ BEGIN
 	PREPARE st_s4 FROM @s4_sql;
 	SET @s5_sql = 'SELECT PlayerId, Name INTO s5_pid, s5_name FROM ih_hall_server_5.Players WHERE UniqueId=unique_id';
 	PREPARE st_s5 FROM @s5_sql;
-	/*SET @s6_sql = 'SELECT PlayerId, Name INTO s6_pid, s6_name FROM ih_hall_server_6.Players WHERE UniqueId=unique_id';
+	SET @s6_sql = 'SELECT PlayerId, Name INTO s6_pid, s6_name FROM ih_hall_server_6.Players WHERE UniqueId=unique_id';
 	PREPARE st_s6 FROM @s6_sql;
 	SET @s7_sql = 'SELECT PlayerId, Name INTO s7_pid, s7_name FROM ih_hall_server_7.Players WHERE UniqueId=unique_id';
 	PREPARE st_s7 FROM @s7_sql;
@@ -62,20 +62,20 @@ BEGIN
 			LEAVE it_loop;
 		END IF;
 		
-		EXECUTE st_s1;
+		/*EXECUTE st_s1;
 		EXECUTE st_s2;
 		EXECUTE st_s3;
 		EXECUTE st_s4;
 		EXECUTE st_s5;
-		/*EXECUTE st_s6;
+		EXECUTE st_s6;
 		EXECUTE st_s7;
 		EXECUTE st_s8;*/
-		/*SELECT PlayerId, Name INTO s1_pid, s1_name FROM ih_hall_server.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s1_pid, s1_name FROM ih_hall_server.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s2_pid, s2_name FROM ih_hall_server_2.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s3_pid, s3_name FROM ih_hall_server_3.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s4_pid, s4_name FROM ih_hall_server_4.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s5_pid, s5_name FROM ih_hall_server_5.Players WHERE UniqueId=unique_id;
-		SELECT PlayerId, Name INTO s6_pid, s6_name FROM ih_hall_server_6.Players WHERE UniqueId=unique_id;
+		/*SELECT PlayerId, Name INTO s6_pid, s6_name FROM ih_hall_server_6.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s7_pid, s7_name FROM ih_hall_server_7.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s8_pid, s8_name FROM ih_hall_server_8.Players WHERE UniqueId=unique_id;*/
 		IF done = 1 THEN
