98e5a261f5b64f03a378ad4443e6c0f3a2b5e592
huoshan017 <huoshan017@hotmail.com>
2019-05-13 17:58:16 +0800

    11111

--- db/get_players.sql
+++ db/get_players.sql
@@ -51,7 +51,7 @@ BEGIN
 		SELECT PlayerId, Name INTO s7_pid, s7_name FROM ih_hall_server_7.Players WHERE UniqueId=unique_id;
 		SELECT PlayerId, Name INTO s8_pid, s8_name FROM ih_hall_server_8.Players WHERE UniqueId=unique_id;*/
 		IF done = 1 THEN
-			done = 0;
+			SET done = 0;
 		END IF;
 		
 		INSERT INTO tmp_players (Account, UniqueId, S1_PID, S1_NAME, S2_PID, S2_NAME, S3_PID, S3_NAME, S4_PID, S4_NAME, S5_PID, S5_NAME/*, S6_PID, S6_NAME, S7_PID, S7_NAME, S8_PID, S8_NAME*/)
