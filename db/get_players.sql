cb9497ed77aa0cec417e2ecd4f1d089f532e34af
huoshan017 <huoshan017@hotmail.com>
2019-05-13 16:41:42 +0800

    11111

--- db/get_players.sql
+++ db/get_players.sql
@@ -6,8 +6,8 @@ BEGIN
     DECLARE account VARCHAR(64);
 	DECLARE unique_id VARCHAR(64);
 	DECLARE last_server_id INT;
-	DECLARE s1_pid, s2_pid, s3_pid, s4_pid, s5_pid, s6_pid, s7_pid, s8_pid INT DEFAULT 0;
-	DECLARE s1_name, s2_name, s3_name, s4_name, s5_name, s6_name, s7_name, s8_name VARCHAR(32) DEFAULT '';
+	DECLARE s1_pid, s2_pid, s3_pid, s4_pid, s5_pid/*, s6_pid, s7_pid, s8_pid*/ INT DEFAULT 0;
+	DECLARE s1_name, s2_name, s3_name, s4_name, s5_name/*, s6_name, s7_name, s8_name*/ VARCHAR(32) DEFAULT '';
     DECLARE done INT DEFAULT 0;
 
 	DECLARE cur CURSOR FOR (SELECT AccountId, UniqueId, LastSelectServerId FROM Accounts);
@@ -26,12 +26,12 @@ BEGIN
 		S4_NAME CHAR(64),
 		S5_PID INT(11) UNSIGNED NOT NULL,
 		S5_NAME CHAR(64),
-		S6_PID INT(11) UNSIGNED NOT NULL,
+		/*S6_PID INT(11) UNSIGNED NOT NULL,
 		S6_NAME CHAR(64),
 		S7_PID INT(11) UNSIGNED NOT NULL,
 		S7_NAME CHAR(64),
 		S8_PID INT(11) UNSIGNED NOT NULL,
-		S8_NAME CHAR(64),
+		S8_NAME CHAR(64),*/
  		PRIMARY KEY (Account)
  	);
 
@@ -41,17 +41,17 @@ BEGIN
 		IF done = 1 THEN
 			LEAVE it_loop;
 		END IF;
-		SELECT PlayerId, Name INTO s1_pid, s1_name FROM ih_hall_server.Players;
-		SELECT PlayerId, Name INTO s2_pid, s2_name FROM ih_hall_server_2.Players;
-		SELECT PlayerId, Name INTO s3_pid, s3_name FROM ih_hall_server_3.Players;
-		SELECT PlayerId, Name INTO s4_pid, s4_name FROM ih_hall_server_4.Players;
-		SELECT PlayerId, Name INTO s5_pid, s5_name FROM ih_hall_server_5.Players;
-		SELECT PlayerId, Name INTO s6_pid, s6_name FROM ih_hall_server_6.Players;
-		SELECT PlayerId, Name INTO s7_pid, s7_name FROM ih_hall_server_7.Players;
-		SELECT PlayerId, Name INTO s8_pid, s8_name FROM ih_hall_server_8.Players;
+		SELECT PlayerId, Name INTO s1_pid, s1_name FROM ih_hall_server.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s2_pid, s2_name FROM ih_hall_server_2.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s3_pid, s3_name FROM ih_hall_server_3.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s4_pid, s4_name FROM ih_hall_server_4.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s5_pid, s5_name FROM ih_hall_server_5.Players WHERE UniqueId=unique_id;
+		/*SELECT PlayerId, Name INTO s6_pid, s6_name FROM ih_hall_server_6.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s7_pid, s7_name FROM ih_hall_server_7.Players WHERE UniqueId=unique_id;
+		SELECT PlayerId, Name INTO s8_pid, s8_name FROM ih_hall_server_8.Players WHERE UniqueId=unique_id;*/
 		
-		INSERT INTO tmp_players (Account, UniqueId, S1_PID, S1_NAME, S2_PID, S2_NAME, S3_PID, S3_NAME, S4_PID, S4_NAME, S4_PID, S4_NAME, S5_PID, S5_NAME, S6_PID, S6_NAME, S7_PID, S7_NAME, S8_PID, S8_NAME)
-		VALUES (account, unique_id, s1_pid, s1_name, s2_pid, s2_name, s3_pid, s3_name, s4_pid, s4_name, s5_pid, s5_name, s6_pid, s6_name, s7_pid, s7_name, s8_pid, s8_name);
+		INSERT INTO tmp_players (Account, UniqueId, S1_PID, S1_NAME, S2_PID, S2_NAME, S3_PID, S3_NAME, S4_PID, S4_NAME, S4_PID, S4_NAME, S5_PID, S5_NAME/*, S6_PID, S6_NAME, S7_PID, S7_NAME, S8_PID, S8_NAME*/)
+		VALUES (account, unique_id, s1_pid, s1_name, s2_pid, s2_name, s3_pid, s3_name, s4_pid, s4_name, s5_pid, s5_name/*, s6_pid, s6_name, s7_pid, s7_name, s8_pid, s8_name*/);
 	END LOOP it_loop;
 	CLOSE cur;
     SELECT * INTO OUTFILE '/tmp/players.xls' FROM tmp_players;
