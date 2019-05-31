fa9b4c8add335a3933526bc83e90ad16852dde17
huoshan017 <huoshan017@hotmail.com>
2019-05-31 14:46:24 +0800

    11111

--- src/login_server/login_server.go
+++ src/login_server/login_server.go
@@ -632,7 +632,7 @@ func login_handler(account, password, channel, client_os, aaid string) (err_code
 				acc_row.SetChannel("facebook")
 				acc_row.SetRegisterTime(int32(now_time.Unix()))
 			}
-			if acc_row.GetPassword() != "" {
+			if acc_row.GetPassword() == "" {
 				acc_row.SetPassword(password)
 			}
 		} else if channel == "guest" {
